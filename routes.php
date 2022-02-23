71e87e95c64ed483dce90d1f5659830ee11cc96d
elmahusuna <elmahusuna02@gmail.com>
2022-02-23 20:26:49 +0700

    jabatanread

--- routes.php
+++ routes.php
@@ -18,10 +18,31 @@ if(isset($_GET['page'])){
         case 'lokasidelete':
             file_exists('pages/admin/lokasidelete.php') ? include 'pages/admin/lokasidelete.php' : include "pages/404.php";
             break;
+
+            // routing jabatan
+            case 'jabatanread':
+                file_exists('pages/admin/jabatan/jabatanread.php') ? include 'pages/admin/jabatan/jabatanread.php' : include 'pages/404.php';  
+                break;
+
+            case 'jabatancreate':
+                file_exists('pages/admin/jabatan/jabatancreate.php') ? include 'pages/admin/jabatan/jabatancreate.php' : include 'pages/404.php';  
+                break;
+
+            case 'jabatanupdate':
+                file_exists('pages/admin/jabatan/jabatanupdate.php') ? include 'pages/admin/jabatan/jabatanupdate.php' : include 'pages/404.php';  
+                break;
+
+            case 'jabatandelete':
+                file_exists('pages/admin/jabatan/jabatandelete.php') ? include 'pages/admin/jabatan/jabatandelete.php' : include 'pages/404.php';  
+                break;
+
+                // routing karyawan
+                case 'karyawanread':
+                    file_exists('pages/admin/karyawan/karyawanread.php') ? include 'pages/admin/karyawan/karyawanread.php' : include 'pages/404.php';  
+                    break;
         default:
         include "pages/404.php";
     }
 }else {
     include "pages/home.php";
 }
-?>
