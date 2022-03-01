40b5a22b81e70fede374601ca598d9ca2e9e652f
elmahusuna <elmahusuna02@gmail.com>
2022-03-01 21:39:53 +0700

    karyawan bagian

--- pages/admin/lokasicreate.php
+++ pages/admin/lokasicreate.php
@@ -1,86 +1,83 @@
-<?php
-    if(isset($_POST['button_create'])){
+<section class="content-header">
+    <div class="content-fluid">
+        <div class="row-mb2">
+    <?php
+    if (isset($_POST['button_create'])) {
 
-        $database = new Database;
+        $database = new Database();
         $db = $database->getConnection();
 
-        // ini untuk validasi input
-        $validasi = "SELECT * FROM lokasi WHERE nama_lokasi = ?";
-        $stmt = $db->prepare($validasi);
+        $validateSQL = "SELECT * FROM lokasi WHERE nama_lokasi=?";
+        $stmt = $db->prepare($validateSQL);
         $stmt->bindParam(1, $_POST['nama_lokasi']);
         $stmt->execute();
 
-        if($stmt->rowCount() > 0 ){
-        ?>
-            <div class="alert alert-danger alert-dismissible">
-                <button class="close" type="button" data-dismiss="alert" aria-hidden="true">x</button>
-                <h5><i class="icon fas fa-check"></i> Gagal </h5>
-                Nama Lokasi Sama sudah ada.
+        if ($stmt->rowCount() > 0) {
+    ?>
+            <div class="alert alert-danger alert-dismissable">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                <h5><i class="fas fa-ban"></i>Gagal</h5>
+                Nama Lokasi sama sudah ada
             </div>
+    <?php
+        } else {
+            $insertSQL = "INSERT INTO lokasi SET nama_lokasi=?";
 
-        <?php
-        }else{
-
-            $insertSQL = "INSERT INTO lokasi SET nama_lokasi = ?";
-            $stmt = $db->prepare($insertSQL);  
+            $stmt = $db->prepare($insertSQL);
             $stmt->bindParam(1, $_POST['nama_lokasi']);
-
-            if($stmt->execute()){
+            if ($stmt->execute()) {
                 $_SESSION['hasil'] = true;
                 $_SESSION['pesan'] = "Berhasil Simpan Data";
-            }else{
+            } else {
                 $_SESSION['hasil'] = false;
                 $_SESSION['pesan'] = "Gagal Simpan Data";
             }
-            echo "<meta http-equiv='refresh' content='0;url=?page=lokasiread'>";
+
+
+            echo "<meta http-equiv='refresh' content='0;url=?page=lokasiread'";
         }
     }
-?>
-
-<!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Lokasi</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item">Lokasi</li>
-              <li class="breadcrumb-item active">Tambah Lokasi</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <div class="content">
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Tambah Lokasi</h3>
-                <a href="?page=lokasiread" class="btn btn-danger btn-sm float-right">
-                Kembali
-                </a>
+    ?>
+    <div class="container-fluid">
+        <div class="row mb2">
+            <div class="col-sm-6">
+                <h1>Tambah Data Lokasi</h1>
             </div>
-
-            <div class="card-body">
-                <form action="" method="post">
-                    <div class="form-group">
-                        <label for="nama_lokasi">Nama Lokasi</label>
-                        <input type="text" name="nama_lokasi" class="form-control">
-                    </div>
-                    <a href="?page=lokasiread" class="btn btn-danger btn-sm float-right">
-                        <i class="fa fa-times"></i> Batal
-                    </a> 
-                    <button type="submit" name="button_create" class="btn btn-success btn-sm float-right">
-                        <i class="fa fa-save"></i> Simpan
-                    </button>
-                </form>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float sm-right">
+                    <li class="breadcrumb-item"><a href="?page=home">Home</a></li>
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="?page=home"> Home </a></li>
+                    <li class="breadcrumb-item"><a href="?page=lokasiread">Lokasi</a></li>
+                    <li class="breadcrumb-item active">Tambah Data</li>
+                </ol>
             </div>
         </div>
     </div>
-    <!-- /.content -->
-<?php include_once "partials/scripts.php" ?>
+</section>
+
+<section class="content">
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-tittle">Tambah Lokasi</h3>
+            <h3 class="card-title">Tambah Lokasi</h3>
+        </div>
+        <div class="card-body">
+            <form method="POST">
+                <div class="form-group">
+                    <label for="nama_lokasi">Nama Lokasi</label>
+                    <input type="text" class="form-control" name="nama_lokasi">
+                    <label for="nama_lokasi">Nama Lokasi*</label>
+                    <input type="text" class="form-control" name="nama_lokasi" required>
+                </div>
+                <a href="?page=lokasiread" class="btn btn-danger btn-sm float-right">
+                    <i class="fa fa-times"></i> Batal
+
+        </div>
+    </div>
+</section>
+<?php include_once "partials/script.php" ?> 
+
+<?php
+include "partials/script.php";
+?>
--- routes.php
+++ routes.php
@@ -1,7 +1,7 @@
 <?php
-if(isset($_GET['page'])){
+if (isset($_GET['page'])) {
     $page = $_GET['page'];
-    switch ($page){
+    switch ($page) {
         case '';
         case 'home':
             file_exists('pages/home.php') ? include 'pages/home.php' : include "pages/404.php";
@@ -20,38 +20,55 @@ if(isset($_GET['page'])){
             break;
 
             // routing jabatan
-            case 'jabatanread':
-                file_exists('pages/admin/jabatan/jabatanread.php') ? include 'pages/admin/jabatan/jabatanread.php' : include 'pages/404.php';  
-                break;
+        case 'jabatanread':
+            file_exists('pages/admin/jabatan/jabatanread.php') ? include 'pages/admin/jabatan/jabatanread.php' : include 'pages/404.php';
+            break;
 
-            case 'jabatancreate':
-                file_exists('pages/admin/jabatan/jabatancreate.php') ? include 'pages/admin/jabatan/jabatancreate.php' : include 'pages/404.php';  
-                break;
+        case 'jabatancreate':
+            file_exists('pages/admin/jabatan/jabatancreate.php') ? include 'pages/admin/jabatan/jabatancreate.php' : include 'pages/404.php';
+            break;
 
-            case 'jabatanupdate':
-                file_exists('pages/admin/jabatan/jabatanupdate.php') ? include 'pages/admin/jabatan/jabatanupdate.php' : include 'pages/404.php';  
-                break;
+        case 'jabatanupdate':
+            file_exists('pages/admin/jabatan/jabatanupdate.php') ? include 'pages/admin/jabatan/jabatanupdate.php' : include 'pages/404.php';
+            break;
 
-            case 'jabatandelete':
-                file_exists('pages/admin/jabatan/jabatandelete.php') ? include 'pages/admin/jabatan/jabatandelete.php' : include 'pages/404.php';  
-                break;
+        case 'jabatandelete':
+            file_exists('pages/admin/jabatan/jabatandelete.php') ? include 'pages/admin/jabatan/jabatandelete.php' : include 'pages/404.php';
+            break;
 
-                // routing karyawan
-                case 'karyawanread':
-                    file_exists('pages/admin/karyawan/karyawanread.php') ? include 'pages/admin/karyawan/karyawanread.php' : include 'pages/404.php';  
-                    break;
-                case 'karyawancreate':
-                    file_exists('pages/admin/karyawan/karyawancreate.php') ? include 'pages/admin/karyawan/karyawancreate.php' : include 'pages/404.php';  
-                    break;
-                case 'karyawanupdate':
-                    file_exists('pages/admin/karyawan/karyawanupdate.php') ? include 'pages/admin/karyawan/karyawanupdate.php' : include 'pages/404.php';  
-                    break;
-                case 'karyawandelete':
-                    file_exists('pages/admin/karyawan/karyawandelete.php') ? include 'pages/admin/karyawan/karyawandelete.php' : include 'pages/404.php';  
-                    break;
+            // routing karyawan
+        case 'karyawanread':
+            file_exists('pages/admin/karyawan/karyawanread.php') ? include 'pages/admin/karyawan/karyawanread.php' : include 'pages/404.php';
+            break;
+        case 'karyawancreate':
+            file_exists('pages/admin/karyawan/karyawancreate.php') ? include 'pages/admin/karyawan/karyawancreate.php' : include 'pages/404.php';
+            break;
+        case 'karyawanupdate':
+            file_exists('pages/admin/karyawan/karyawanupdate.php') ? include 'pages/admin/karyawan/karyawanupdate.php' : include 'pages/404.php';
+            break;
+        case 'karyawandelete':
+            file_exists('pages/admin/karyawan/karyawandelete.php') ? include 'pages/admin/karyawan/karyawandelete.php' : include 'pages/404.php';
+            break;
+        case 'karyawanbagian':
+            file_exists('pages/admin/karyawan/karyawanbagian.php') ? include 'pages/admin/karyawan/karyawanbagian.php' : include 'pages/404.php';
+            break;
+
+            // Bagian
+        case 'bagianread':
+            file_exists('pages/admin/bagianread.php') ? include 'pages/admin/bagianread.php' : include "pages/404.php";
+            break;
+        case 'bagiancreate':
+            file_exists('pages/admin/bagiancreate.php') ? include 'pages/admin/bagiancreate.php' : include "pages/404.php";
+            break;
+        case 'bagianupdate':
+            file_exists('pages/admin/bagianupdate.php') ? include 'pages/admin/bagianupdate.php' : include "pages/404.php";
+            break;
+        case 'bagiandelete':
+            file_exists('pages/admin/bagiandelete.php') ? include 'pages/admin/bagiandelete.php' : include "pages/404.php";
+            break;
         default:
-        include "pages/404.php";
+            include "pages/404.php";
     }
-}else {
+} else {
     include "pages/home.php";
 }
